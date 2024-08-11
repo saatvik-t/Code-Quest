62c6385f00cf02dd66917dc443468bde5de99256
Saatvik Tammishetty <saatvik.tammishetty@gmail.com>
2024-08-11 22:04:49 +0530

    Configured MongoDB Connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
+
+dotenv.config({ path: '../.env' })
 
 const DBConnection = async () => {
-    const MONGO_URI = process.env.MONGODB_URI;
     try {
+        const MONGO_URI = process.env.MONGODB_URL;
         await mongoose.connect(MONGO_URI);
-        console.log("Database Connection is established !");
-    } catch(error) {
-        console.log("Error while connecting to MongoDB : ", error);
+        console.log('Database Connection is established !');
+    } catch (error) {
+        console.log('Error while connecting to MongoDB : ', error.message);
+        process.exit(1);
     }
-};
+}
 
-module.exports = {DBConnection};
+module.exports = { DBConnection };
